72134b52a095bd9585bb404c32ea3f5ffec83873
Agselll <110612804+Agselll@users.noreply.github.com>
2023-10-11 20:39:52 +0800

    Add files via upload
    Signed-off-by: Agselll <110612804+Agselll@users.noreply.github.com>

---
+++ posttest4/navbar.php
@@ -0,0 +1,7 @@
+    <div class="navbar">
+      <ul>
+        <li><a id="about" href="RDS.php">Beranda</a></li>
+        <li><a href="portfolio.php" >About</a></li>
+        <li><a href="input.php">Input</a></li>
+      </ul>
+    </div>
